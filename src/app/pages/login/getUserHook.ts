76fe67337bb083cf035edd550763dd20a5059a2f
dabin <ldabinn@gmail.com>
2024-07-04 18:09:58 +0900

    리액트 쿼리 유저정보 훅 추가

---
+++ src/app/pages/login/getUserHook.ts
@@ -0,0 +1,31 @@
+import { authService } from "@/app/Firebase"
+import { QueryObserverResult, useQuery } from "@tanstack/react-query"
+
+export interface userData {
+    displayName: string;
+    email: string;
+    photoURL: string;
+    uid: string;
+  }
+
+
+const getuser = () => {
+    return new Promise((resolve, reject) => {
+        authService.onAuthStateChanged((user) => {
+            if (user) {
+                resolve(user)
+            } else {
+                reject(new Error("유저 정보가 없습니다."))
+            }
+        })
+    })
+}
+
+const userQueryHook = () => {
+    const { data, isPending, isError, error,refetch}:QueryObserverResult<userData> = useQuery({
+        queryKey: ['getuser'],
+        queryFn: getuser,
+      });
+      return {data,isPending,isError,error,refetch}
+}
+export default userQueryHook;
