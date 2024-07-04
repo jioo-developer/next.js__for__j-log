80440883c6b4f777522ded2b47ee6421cf13c111
dabin <ldabinn@gmail.com>
2024-07-04 18:09:47 +0900

    메인페이지/로그인페이지/루트페이지 수정

--- src/app/pages/main/page.tsx
+++ src/app/pages/main/page.tsx
@@ -1,10 +1,22 @@
 'use client'
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import userQueryHook from '../login/getUserHook';
+import { useRouter } from 'next/navigation';
 const MainPage = () => {
+ const { data } = userQueryHook(); 
+  const router = useRouter();
+
+  useEffect(()=>{
+    if(data) {
+      router.push('/pages/main')
+    } else {
+      router.push('/pages/login')
+    }
+  },[])
+  
   return (
     <div>
-     123
+     메인
     </div>
   )
 }
