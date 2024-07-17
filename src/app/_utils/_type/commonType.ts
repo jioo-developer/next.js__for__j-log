21f70cdfe7c065a5ff9b9a14cee35708382b3dc1
dabin <ldabinn@gmail.com>
2024-07-17 18:44:45 +0900

    공용 타입 추가

--- src/app/_utils/_type/commonType.ts
+++ src/app/_utils/_type/commonType.ts
@@ -3,3 +3,9 @@ import { ReactNode } from "react"
 export interface childrenProps {
   children : ReactNode
 }
+
+export interface styleProps {
+  width: number;
+  height?: number | undefined;
+  fontSize:number;
+}
