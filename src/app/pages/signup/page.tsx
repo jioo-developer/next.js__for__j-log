ac413792fa7e983b90ca4e0e65c05c18ae96ced8
jioo <rlawl3333@gmail.com>
2024-08-12 17:31:26 +0900

    회원가입 페이지 코드 추가

---
+++ src/app/pages/signup/page.tsx
@@ -0,0 +1,127 @@
+"use client";
+import "@/app/_asset/auth.scss";
+import { FormEvent, useState } from "react";
+import { Input } from "@/stories/atoms/Input";
+import { Button } from "@/stories/atoms/Button";
+import Image from "next/image";
+import Checker from "@/stories/atoms/Checker";
+import useLoadNickName from "@/app/hooks/signup/getNicknamehooks";
+import { Popup } from "@/stories/atoms/Popup";
+import { popupMessageStore } from "@/app/store/common";
+import signupHandler from "@/app/hooks/signup/signuphooks";
+import { useRouter } from "next/navigation";
+import useUserQueryHook from "@/app/hooks/login/getUserHook";
+
+const authData = [
+  { id: "auth", text: "회원가입및 운영약관 동의", important: true },
+  { id: "data", text: "개인정보 수집 및 동의", important: true },
+  { id: "location", text: "위치정보 이용약관 동의", important: false },
+];
+
+const SignupPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [nickname, setNickname] = useState("");
+
+  const [disable, setDisable] = useState(false);
+  const msg = popupMessageStore();
+  const { data } = useLoadNickName();
+  const { refetch } = useUserQueryHook();
+  const router = useRouter();
+
+  function isNickName(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (data) {
+      const overlapFilter = data.some((item) => {
+        return Object.values(item).includes(nickname);
+      });
+      if (overlapFilter) {
+        popupMessageStore.setState({ message: "이미 사용중인 닉네임 입니다." });
+        setNickname("");
+      } else {
+        createAccount(email, password, nickname);
+      }
+    }
+  }
+
+  async function createAccount(
+    email: string,
+    password: string,
+    nickname: string
+  ) {
+    const signupResult = await signupHandler(email, password, nickname);
+    if (signupResult !== null || signupResult !== undefined) {
+      refetch();
+      router.push("/pages/main");
+      popupMessageStore.setState({ message: "회원가입을 환영합니다." });
+    } else {
+      popupMessageStore.setState({
+        message: "회원가입 중 오류가 발생했습니다.",
+      });
+    }
+  }
+
+  return (
+    <div className="Auth_wrap">
+      <div className="title_area">
+        <button onClick={() => router.push("/")}>
+          <Image
+            src="/img/backbtn.svg"
+            className="close"
+            width={30}
+            height={30}
+            alt=""
+          />
+        </button>
+        <p>회원가입</p>
+      </div>
+      <form className="auth-form" onSubmit={(e) => isNickName(e)}>
+        <p className="id_title">
+          이메일&nbsp;<span>*</span>
+        </p>
+        <Input
+          type="id"
+          width={375}
+          height={45}
+          fontSize={16}
+          setstate={setEmail}
+        />
+        <p className="warning">
+          ※ 실제 사용하시는 이메일을 사용하셔야 비밀번호를 찾으실 수 있습니다.
+        </p>
+        <p className="id_title">
+          비밀번호&nbsp;<span>*</span>
+        </p>
+        <Input
+          type="password"
+          width={375}
+          height={45}
+          fontSize={16}
+          setstate={setPassword}
+        />
+        <p className="id_title">
+          닉네임 &nbsp;<span>*</span>
+        </p>
+        <Input
+          type="text"
+          width={375}
+          height={45}
+          fontSize={16}
+          setstate={setNickname}
+        />
+        <Checker allcheck items={authData} setState={setDisable} />
+        <Button
+          width={"full"}
+          theme="primary"
+          disable={disable}
+          className={disable ? "un_btn" : "btn"}
+        >
+          회원가입
+        </Button>
+      </form>
+      {msg.message !== "" && <Popup rightAlign top />}
+    </div>
+  );
+};
+
+export default SignupPage;
