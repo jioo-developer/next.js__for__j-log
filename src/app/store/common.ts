0727f5666a48856abf34b998e473ad185f9e726d
jioo <rlawl3333@gmail.com>
2024-08-22 01:38:37 +0900

    팝업 zutand state 객체 옵션 추가

--- src/app/store/common.ts
+++ src/app/store/common.ts
@@ -2,6 +2,8 @@ import create from "zustand";
 
 export const popupMessageStore = create(() => ({
   message: "",
+  type: "alert",
+  clickFunc: () => {},
 }));
 
 export const globalRefetch = create(() => ({
