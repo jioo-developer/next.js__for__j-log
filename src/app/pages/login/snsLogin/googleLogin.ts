67369c63b48332e674862be61c92160182b3796c
jioo <rlawl3333@gmail.com>
2024-08-22 15:42:32 +0900

    구글로그인 로직 수정

--- src/app/pages/login/snsLogin/googleLogin.ts
+++ src/app/pages/login/snsLogin/googleLogin.ts
@@ -1,54 +1,36 @@
 import { authService, db } from "@/app/Firebase";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { popupMessageStore } from "@/app/store/common";
+import { objType } from "./sosialLogin";
 
 export async function onGoogle() {
   const provider = new GoogleAuthProvider();
-  const result = await signInWithPopup(authService, provider);
-  const userID = result.user.uid;
-  const userName = result.user.displayName;
-  if (result.user) {
-    if (!isSecondpw(userID)) {
-      const password = window.prompt(
-        "회원 탈퇴에 사용 될 비밀번호를 입력해주세요."
-      );
-      if (password) {
-        secondPassword(userID, userName, parseInt(password));
-      } else {
-        authService.signOut();
-      }
-    } else {
-      window.location.href = "/";
-    }
-  } else {
-    popupMessageStore.setState({ message: "로그인 정보를 찾을 수 없습니다." });
-  }
-  // 함수 끝
+  const result = (await signInWithPopup(authService, provider)).user;
+  const { userId, userName, service } = {
+    userId: result.uid,
+    userName: result.displayName as string,
+    service: result.providerData[0].providerId,
+  };
+  return { userId, userName, service };
 }
 
-async function isSecondpw(userId: string) {
-  const docRef = await doc(db, "nickname", userId);
+export async function isSecondpw(id: string) {
+  const docRef = await doc(db, "nickname", id);
   const docSnap = await getDoc(docRef);
-  if (docSnap.exists()) {
-    return docSnap.data();
+  if (docSnap.exists() && docSnap.data().service) {
+    return true;
+  } else {
+    return false;
   }
 }
 
-async function secondPassword(
-  id: string,
-  nickname: string | null,
-  password: number
-) {
-  try {
-    await setDoc(doc(db, "nickname", id), {
-      id: id,
-      password: password,
-      nickname: nickname ? nickname : id,
-      // 닉네임을 넣는 이유는 해당 uid가 어떤 유저의 것인지 알기 위함
-      // 만약 nickname이 null 일 때 uid를 대신 넣어주기
-    }).then(() => (window.location.href = "/"));
-  } catch (error) {
-    console.log(error);
-  }
+export async function secondPassword(params: objType) {
+  await setDoc(doc(db, "nickname", params.id), {
+    id: params.id,
+    password: params.pw,
+    nickname: params.name,
+    service: params.service,
+    // 닉네임을 넣는 이유는 해당 uid가 어떤 유저의 것인지 알기 위함
+    // 만약 nickname이 null 일 때 uid를 대신 넣어주기
+  });
 }
--- src/app/pages/login/snsLogin/sosialLogin.tsx
+++ src/app/pages/login/snsLogin/sosialLogin.tsx
@@ -1,10 +1,84 @@
-import { onGoogle } from "@/app/pages/login/snsLogin/googleLogin";
+import {
+  popupInit,
+  popuprHandler,
+} from "@/app/common/handler/error/ErrorHandler";
+import {
+  isSecondpw,
+  onGoogle,
+  secondPassword,
+} from "@/app/pages/login/snsLogin/googleLogin";
+import { popupMessageStore } from "@/app/store/common";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export type objType = {
+  id: string;
+  name: string;
+  service: string;
+  pw?: number;
+};
 
 const SocialLoginPage = () => {
+  const [pw, setPw] = useState("");
+  const [userObj, setObj] = useState<objType>({
+    id: "",
+    name: "",
+    service: "",
+  });
+
+  const router = useRouter();
+  const ispopClick = popupMessageStore().isClick;
+
+  useEffect(() => {
+    if (ispopClick) {
+      secondSubmit();
+    }
+  }, [ispopClick]);
+
+  async function LoginHandler() {
+    try {
+      const googleUser = await onGoogle();
+      const isUserSecondPw = await isSecondpw(googleUser.userId);
+      if (!isUserSecondPw) {
+        popuprHandler({
+          message: "회원탈퇴에 사용 될 2차 비밀번호를 입력해주세요.",
+          type: "prompt",
+          state: setPw,
+        });
+        setObj({
+          id: googleUser.userId,
+          name: googleUser.userName,
+          service: googleUser.service,
+        });
+      } else {
+        router.push("/pages/main");
+      }
+    } catch {
+      popuprHandler({
+        message: "로그인 정보가 조회 되지 않습니다",
+      });
+    }
+  }
+
+  async function secondSubmit() {
+    const newObj = { ...userObj };
+    newObj.pw = parseInt(pw);
+    popupInit();
+    try {
+      await secondPassword(newObj);
+      router.push("/pages/main");
+    } catch {
+      popuprHandler({
+        message: "2차비밀번호 설정중 에러가 발생하였습니다",
+      });
+    }
+  }
+
   return (
     <div className="sns_sign">
-      <button className="sns-btn" name="google" onClick={() => onGoogle()}>
+      <div className="blind"></div>
+      <button className="sns-btn" name="google" onClick={LoginHandler}>
         <Image src="/img/google.svg" alt="구글 로그인" width={20} height={20} />
         <figcaption className="btn_title">구글로 시작하기</figcaption>
       </button>
