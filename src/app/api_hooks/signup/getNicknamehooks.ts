32e9d450bd28332b7ef3ae8aed1430ae2d085cb9
jioo <rlawl3333@gmail.com>
2024-08-29 14:01:55 +0900

    nickname 관련 hook 추가

--- src/app/api_hooks/signup/getNicknamehooks.ts
+++ src/app/api_hooks/signup/getNicknamehooks.ts
@@ -3,29 +3,29 @@ import { QueryObserverResult, useQuery } from "@tanstack/react-query";
 import { collection, getDocs } from "firebase/firestore";
 
 async function getNickName() {
-  try {
-    const snapshot = await getDocs(collection(db, "nickname"));
-    const data = snapshot.docs.map((doc) => ({ ...doc.data() }));
-
-    if (data.length > 0) {
-      return data;
-    } else {
-      throw new Error("사용 가능한 닉네임 입니다.");
-    }
-  } catch (error) {
-    throw new Error("닉네임이 조회 되지 않습니다.");
+  const snapshot = await getDocs(collection(db, "nickname"));
+  if (snapshot.docs.length > 0) {
+    return snapshot.docs.map((doc) => ({ ...doc.data() }));
+  } else {
+    throw new Error("닉네임 DB가 조회되지 않습니다");
   }
 }
 
-const useNickQueryHook = () => {
-  const { data, error }: QueryObserverResult<string[], Error> = useQuery({
-    queryKey: ["getNickname"],
-    queryFn: getNickName,
-  });
+const useNameQueryHook = () => {
+  const { data, error, isLoading }: QueryObserverResult<string[], Error> =
+    useQuery({
+      queryKey: ["getNickname"],
+      queryFn: getNickName,
+      select: (data) => {
+        return data.map((item) => item.nickname);
+      },
+      staleTime: 1 * 60 * 1000, // 1분
+      notifyOnChangeProps: ["data"],
+    });
 
   const nicknameData = data;
 
-  return { nicknameData, error };
+  return { nicknameData, error, isLoading };
 };
 
-export default useNickQueryHook;
+export default useNameQueryHook;
