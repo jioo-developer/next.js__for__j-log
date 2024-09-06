c87de4762a56655f24892e38bb80479b464ee8a6
jioo <rlawl3333@gmail.com>
2024-09-06 20:34:21 +0900

    공용 함수 수정

---
+++ src/app/handler/commonHandler.ts
@@ -0,0 +1,21 @@
+const time = new Date();
+export const timeData = {
+  year: time.getFullYear(),
+  month: time.getMonth() + 1,
+  day: time.getDate(),
+};
+
+export function cookieHandler(name: string) {
+  const time = new Date();
+  const result = new Date(
+    time.getFullYear(),
+    time.getMonth(),
+    time.getDate(),
+    23,
+    59,
+    59
+  );
+  result.setMilliseconds(999);
+  result.setHours(result.getHours() + 9);
+  document.cookie = `${name}-Cookie=${encodeURIComponent("done")}; expires=${result.toUTCString()};`;
+}
