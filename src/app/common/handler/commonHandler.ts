a604fd37d89c047e2b72874bd02cb73071456655
jioo <rlawl3333@gmail.com>
2024-09-06 20:31:13 +0900

    삭제파일 추가

--- src/app/common/handler/commonHandler.ts
+++
@@ -1,6 +0,0 @@
-const time = new Date();
-export const timeData = {
-  year: time.getFullYear(),
-  month: time.getMonth() + 1,
-  day: time.getDate(),
-};
--- src/app/common/handler/useInput.tsx
+++
@@ -1,9 +0,0 @@
-import { ChangeEvent, useState } from "react";
-
-export const useInput = (initialValue: string | number) => {
-  const [value, setValue] = useState(initialValue);
-  const valueChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
-  };
-  return { value, valueChangeHandler };
-};
