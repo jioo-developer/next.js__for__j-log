a94ebfa7888dc2be42ac9f48a4064124f5ad5ba2
jioo <rlawl3333@gmail.com>
2024-10-13 15:00:36 +0900

    메인페이지 코드 수정정

--- src/app/pages/main/page.tsx
+++ src/app/pages/main/page.tsx
@@ -1,43 +1,36 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import userQueryHook from "@/app/api_hooks/login/getUserHook";
-import { useRouter } from "next/navigation";
 import SkeletonItem from "@/app/components/SkeletonItem";
 import PostItem from "@/app/components/PostItem";
 import usePostQueryHook from "@/app/api_hooks/main/getPostHook";
 import { searchStore } from "@/app/store/common";
 import { FirebaseData } from "@/app/api_hooks/detail/getDetailHook";
-import { isSecondaryPw } from "@/app/api_hooks/login/snsLogin/googleLogin";
-import { popupInit, popuprHandler } from "@/app/handler/error/ErrorHandler";
+import { usePathname, useRouter } from "next/navigation";
 
 const MainPage = () => {
   const [postState, setState] = useState<FirebaseData[]>([]);
 
-  const router = useRouter();
-
   const { data, isLoading } = userQueryHook();
   const { postData } = usePostQueryHook();
-
-  useEffect(() => {
-    if (isLoading) {
-      popuprHandler({ message: "회원정보를 불러 오는 중입니다." });
-    } else {
-      popupInit();
-      if (!data) {
-        router.push("/pages/login");
-      } else if (data) {
-        const result = isSecondaryPw(data.uid);
-        if (!result) {
-          router.push("/pages/login");
-        }
-      }
-    }
-  }, [data, isLoading]);
 
   const searchInfo = {
     params: searchStore().searchText,
     isSearch: searchStore().searchText !== "" ? true : false,
   };
+
+  const router = {
+    pathname: usePathname(),
+    handler: useRouter(),
+  };
+
+  const rootPath = router.pathname === "/";
+
+  useEffect(() => {
+    if (rootPath) {
+      window.location.href = "/pages/main";
+    }
+  }, []);
 
   useEffect(() => {
     if (searchInfo.isSearch) {
@@ -50,16 +43,20 @@ const MainPage = () => {
   }, [searchInfo.isSearch]);
 
   const fallbackHandler = () => {
-    return [1, 2, 3].map((item) => {
-      return <SkeletonItem key={item} />;
-    });
+    if (!rootPath) {
+      return [1, 2, 3].map((item) => {
+        return <SkeletonItem key={item} />;
+      });
+    }
   };
 
   const showDataHandler = () => {
-    const array = searchInfo.isSearch ? postState : postData;
-    return array.map((item, index) => {
-      return <PostItem item={item} index={index} key={index} />;
-    });
+    if (data) {
+      const array = searchInfo.isSearch ? postState : postData;
+      return array.map((item, index) => {
+        return <PostItem item={item} index={index} key={index} />;
+      });
+    }
   };
 
   return (
