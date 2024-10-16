c1a406fa7d793956dfaa30fc7afe33c0a9af2633
jioo <rlawl3333@gmail.com>
2024-10-16 18:42:13 +0900

    onfileChange 함수 테스트 코드 작성

--- src/app/handler/file/onFileChangeHandler.ts
+++ src/app/handler/file/onFileChangeHandler.ts
@@ -1,25 +1,29 @@
 async function onFileChange(files: File[]) {
-  const result = await Promise.all(
-    files.map((item: File) => {
-      return new Promise<string>((resolve, reject) => {
-        const reader = new FileReader();
-        // load
-        reader.readAsDataURL(item);
-        // create
-        reader.onloadend = (e) => {
-          // end
-          const resultItem = e.target as FileReader;
-          if (resultItem) {
-            const dataURL = resultItem.result as string;
-            resolve(dataURL);
-          } else {
-            reject(false);
-          }
-        };
-      });
-    })
-  );
-  return { result, files };
+  if (files.length > 0) {
+    const result = await Promise.all(
+      files.map((item: File) => {
+        return new Promise<string>((resolve, reject) => {
+          const reader = new FileReader();
+          // load
+          reader.readAsDataURL(item);
+          // create
+          reader.onloadend = (e) => {
+            // end
+            const resultItem = e.target as FileReader;
+            if (resultItem) {
+              const dataURL = resultItem.result as string;
+              resolve(dataURL);
+            } else {
+              reject(false);
+            }
+          };
+        });
+      })
+    );
+    return { result, files };
+  } else {
+    throw new Error("유효한 파일이 없습니다.");
+  }
 }
 
 export default onFileChange;
