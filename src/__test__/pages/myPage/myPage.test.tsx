205839f0f42934c7adef5c4f2a5345a52e52626d
jioo <rlawl3333@gmail.com>
2024-10-20 20:06:09 +0900

    my페이지 테스트 코드 수정

--- src/__test__/pages/myPage/myPage.test.tsx
+++ src/__test__/pages/myPage/myPage.test.tsx
@@ -15,7 +15,8 @@ import { popuprHandler } from "@/app/handler/error/ErrorHandler";
 import { updateProfile, User } from "firebase/auth";
 import { doc, updateDoc } from "firebase/firestore";
 import { db } from "@/app/Firebase";
-
+import storageUpload from "@/app/handler/file/storageUploadHandler";
+import onFileChange from "@/app/handler/file/onFileChangeHandler";
 jest.mock("@/app/Firebase", () => ({
   authService: {
     currentUser: {
@@ -68,9 +69,13 @@ jest.mock("@/app/api_hooks/common/getnameHook", () => ({
   }),
 }));
 
+jest.mock("@/app/handler/file/onFileChangeHandler", () => jest.fn());
+
 jest.mock("@/app/handler/mypage/useMutationHandler", () => jest.fn());
 
 (useNameChanger as jest.Mock).mockReturnValue({ mutate: jest.fn() });
+
+jest.mock("@/app/handler/file/storageUploadHandler", () => jest.fn());
 
 describe("닉네임 변경 로직 테스트", () => {
   beforeEach(async () => {
@@ -163,7 +168,33 @@ describe("닉네임 변경 로직 테스트", () => {
       }
     );
   });
-  test("닉네임 변경 함수 실패 테스트", async () => {
+  test("닉네임 변경 실패 테스트", async () => {
+    const queryClient = new QueryClient();
+    const handler = jest.requireActual(
+      "@/app/handler/mypage/useMutationHandler"
+    ).default;
+    const { result } = renderHook(() => handler(), {
+      wrapper: ({ children }) => (
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      ),
+    });
+
+    const obj = {
+      data: null,
+      nickname: "NewNick",
+    };
+
+    await act(async () => {
+      result.current.mutate(obj);
+    });
+
+    await waitFor(() => {
+      popuprHandler({ message: "닉네임을 변경에 실패하였습니다." });
+    });
+  });
+  test("변경 하려는 닉네임이 이미 존재 할 때 테스트", async () => {
     const { nicknameData } = useNameQueryHook();
     const isNamecheck = nicknameData.includes("Nickname1");
     expect(isNamecheck).toBe(true);
@@ -174,8 +205,71 @@ describe("닉네임 변경 로직 테스트", () => {
     }
   });
 });
+describe("파일 업로드 성공 및 실패 테스트", () => {
+  const mockFiles: File[] = [
+    new File(["file content 1"], "file1.txt", { type: "text/plain" }),
+    new File(["file content 2"], "file2.txt", { type: "text/plain" }),
+  ];
+  const mockFilePaths = ["file1.txt", "file2.txt"]; // 파일 경로 또는 이름
+  beforeEach(async () => {
+    jest.clearAllMocks();
+  });
+  // 성공 테스트
+  test("파일 업로드 성공 시 storageUpload가 호출 테스트", async () => {
+    // onFileChange와 storageUpload가 정상적으로 성공하도록 설정
+    (onFileChange as jest.Mock).mockResolvedValue({
+      result: mockFilePaths,
+      files: mockFiles,
+    });
+
+    (storageUpload as jest.Mock).mockResolvedValue("uploadSuccess");
+
+    // 함수 실행
+    await onFileChange(mockFiles);
+    await storageUpload(mockFilePaths, mockFiles);
+
+    // storageUpload가 호출되었는지 확인
+    expect(storageUpload).toHaveBeenCalledWith(mockFilePaths, mockFiles);
+
+    expect(popuprHandler).not.toHaveBeenCalled();
+  });
+
+  // 실패 테스트
+  test("파일 업로드 실패 시 popuprHandler 호출 테스트", async () => {
+    const errorMessage = "프로필 업로드에 실패하였습니다.";
+
+    // onFileChange는 성공, storageUpload는 실패하도록 설정
+    (onFileChange as jest.Mock).mockResolvedValue({
+      result: mockFilePaths,
+      files: mockFiles,
+    });
+
+    (storageUpload as jest.Mock).mockRejectedValue(new Error(errorMessage));
+
+    await act(async () => {
+      try {
+        // 함수 실행
+        await onFileChange(mockFiles);
+        await storageUpload(mockFilePaths, mockFiles);
+      } catch {
+        popuprHandler({ message: errorMessage });
+      }
+    });
+
+    await waitFor(() => {
+      // popuprHandler가 호출되었는지 확인
+      expect(popuprHandler).toHaveBeenCalledWith({
+        message: errorMessage,
+      });
+    });
+  });
+});
+
 describe("프로필 이미지 변경 로직 테스트", () => {
   const upload = ["uploadedUrl"];
+  beforeEach(async () => {
+    jest.clearAllMocks();
+  });
   test("프로필 이미지 변경 성공 테스트", async () => {
     // updateProfile을 성공적으로 호출하고, 라우터가 호출되는지 확인
     await act(async () => {
