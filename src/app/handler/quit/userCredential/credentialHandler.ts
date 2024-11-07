599a04bee7ca9b216e50016821961b200d3ce822
jioo <rlawl3383@gmail.com>
2024-11-07 18:29:18 +0900

    credentialhandler 코드 전면 축소

--- src/app/handler/quit/userCredential/credentialHandler.ts
+++ src/app/handler/quit/userCredential/credentialHandler.ts
@@ -1,31 +1,9 @@
-import { db } from "@/app/Firebase";
 import { User } from "firebase/auth";
-import { doc, getDoc } from "firebase/firestore";
-import { popupMessageStore } from "@/app/store/common";
-import { popuprHandler } from "../../error/ErrorHandler";
-
-async function isLoginType(user: User) {
-  const docRef = doc(db, "nickname", user.uid);
-  const docSnap = await getDoc(docRef);
-  if (docSnap.exists()) {
-    if (docSnap.data().service !== "password") {
-      return "sosial";
-    } else {
-      return "origin";
-    }
-  } else {
-    popuprHandler({ message: "회원탈퇴 도중 에러가 발생하였습니다" });
-    return null;
-  }
-}
 
 export async function isCredential(user: User) {
-  try {
-    const accountType = await isLoginType(user);
-    popupMessageStore.setState({ isClick: false });
-    return accountType;
-  } catch {
-    popuprHandler({ message: "회원탈퇴 도중 에러가 발생하였습니다" });
-    return null;
+  if (user.providerData[0].providerId === "password") {
+    return "origin";
+  } else {
+    return "sosial";
   }
 }
